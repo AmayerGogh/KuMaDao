4fbcb65ba3b65ce4aadce250de9f9e3dee961f2a
AmayerGogh <454593034@qq.com>
2017-02-13 12:37:47 +0800

    添加comclass

--- Amayer.Info.CL/Com/WebHelper.cs
+++ Amayer.Info.CL/Com/WebHelper.cs
@@ -8,14 +8,28 @@ using System.Threading.Tasks;
 using System.Web.Mvc;
 using System.IO;
 using System.Configuration;
-
+using Amayer.Info.CL.ComMsg;
 
 namespace Amayer.Info.CL.Com
 {
     public class WebHelper
     {
+        public delegate int DoEventHandlers(Object o);
+        public Result TryDoSomething(DoEventHandlers db, Object o)
+        {
+            var result = new Result();
+            try
+            {
+                result.Status = db(o);
+            }
+            catch (Exception ex)
+            {
+                result.Status = -1;
+                result.Message = ex.ToString();
+            }
+            return result;
+        }
 
-      
 
 
     }
---
+++ Amayer.Info.CL/ComMsg/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amayer.Info.CL.ComMsg
+{
+  public  class Result
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+    }
+}
